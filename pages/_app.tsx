890483046dc492e3fdb74c724429ff274a1f689a
jeffascript <ofoaro.jeff@gmail.com>
2021-06-15 03:57:29 +0200

    [feature] layout for wrapping the home route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import { AppProps } from "next/app";
-// import Head from "next/head";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title> House Listings | By Jeff</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        {/*/favicon.ico  will work since it is on the public folder as our baseurl is . on the settings */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,49 @@
-// import { FunctionComponent, ReactNode } from "react";
-// import Link from "next/link";
+import React, { FunctionComponent, ReactNode } from "react";
+import Link from "next/link";
 // import { useAuth } from "src/auth/useAuth";
 
-export {};
+export interface LayoutProps {
+  main: ReactNode;
+}
+
+const Layout: FunctionComponent<LayoutProps> = ({ main }) => {
+  const authenticated = false;
+
+  return (
+    <React.Fragment>
+      <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+        <nav className="bg-gray-800 h-16">
+          <div className="px-6 flex items-center justify-between h-16">
+            <Link href="/">
+              <a>
+                <img
+                  src="/home-color.svg"
+                  alt="home house"
+                  className="inline-block w-6"
+                />
+              </a>
+            </Link>
+
+            {authenticated ? (
+              <>
+                <Link href="/houses/add">
+                  <a>Add House</a>
+                </Link>
+                <button onClick={() => {}}>Logout</button>
+              </>
+            ) : (
+              <>
+                <Link href="/auth">
+                  <a> Login / Signup</a>
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+        {main}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
