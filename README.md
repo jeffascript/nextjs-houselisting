635dec9119585f69f4707beb78a085dacc7575d1
jeffascript <ofoaro.jeff@gmail.com>
2021-06-16 02:32:43 +0200

    [Auth] firebaseAuthUI init. for login

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Home Sweet Home
+# House Listings in Berlin
+
+# TODO
+
+[ ] - restyle the app with glassmorphism design
+
+[ ] - restyle the signin firebaseui with global css reset (firebaseAuth.tsx)
+
+```
+https://github.com/firebase/firebaseui-web-react/tree/e2acfddcfa6fca369eecc7184f8830a9c018631a/example/src
+https://www.npmjs.com/package/react-firebaseui
+
+
+```
--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,50 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FunctionComponent, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+/**
+ * docs: https://github.com/firebase/firebaseui-web-react
+ */
+
+const firebaseAuthUIConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  // Redirect to / after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  /**
+   * Since StyledFirebaseAuth never likes to be rendered on the SSR, so we can use the state/useeffect renderer to hack it
+   */
+
+  const [renderAuth, setRenderAuth] = useState<boolean>(false);
+
+  useEffect(() => {
+    setRenderAuth(true);
+  }, []);
+
+  return (
+    <div className="mt-16">
+      {renderAuth ? (
+        <StyledFirebaseAuth
+          uiConfig={firebaseAuthUIConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
