1c9295de5a555b291358fcccacac6dce6c6049e9
jeffascript <ofoaro.jeff@gmail.com>
2021-06-17 08:32:00 +0200

    [map] mapbox rendered init. on homepage

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 # House Listings in Berlin
 
+## Start in DEV
+
+```
+npm run dev
+
+```
+
+OR
+
+```
+yarn run dev
+```
+
+## Start in PRD
+
+```
+npm start
+
+```
+
+OR
+
+```
+yarn start
+```
+
 # TODO
 
 [ ] - restyle the app with glassmorphism design
 
 [ ] - restyle the signin firebaseui with global css reset (firebaseAuth.tsx)
+
+[ ] - populate postgres with data from listing API Berlin.
+
+[ ] - Styling for Darkmode and lightmode
 
 ```
 https://github.com/firebase/firebaseui-web-react/tree/e2acfddcfa6fca369eecc7184f8830a9c018631a/example/src
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,38 @@
-// import { useRef, useState } from "react";
-// import Link from "next/link";
-// import { Image } from "cloudinary-react";
-// import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { Image } from "cloudinary-react";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery_houses } from "src/generated/HousesQuery";
 // import { SearchBox } from "./searchBox";
 
-export {};
+export interface IMapProps {}
+
+const Map: React.FC<IMapProps> = () => {
+  const mapRef = useRef<ReactMapGL | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 52.51126239232381,
+    longitude: 13.288278216771987,
+    zoom: 10,
+  });
+
+  return (
+    <div className="text-black relative">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="calc(100vh - 64px)"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        ref={(instance) => (mapRef.current = instance)} //store this instance of this map on the ref
+        minZoom={5}
+        maxZoom={16}
+        mapStyle="mapbox://styles/jeffemerald/ckq0iiirc135u17p8d60mwvyw"
+        // mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef
+      />
+    </div>
+  );
+};
+
+export default Map;
