224159f9b4c40ad2142bf367d789b2f6a5c561a6
jeffascript <ofoaro.jeff@gmail.com>
2021-06-19 01:54:24 +0200

    [house] add house + img preview done w/cond.render

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
@@ -70,84 +70,88 @@ const HouseFormData: React.FC<IHouseFormDataProps> = () => {
         />
         {errors.address && <p> {errors.address.message}</p>}
         {/* WATCH IN ACTION */}
-        <h2>{address}</h2>
+        {/* <h2>{address}</h2> */}
       </div>
-      <div className="mt-4">
-        <label
-          htmlFor="image"
-          className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
-        >
-          <div className="flex flex-col justify-evenly items-center">
-            <FiUploadCloud />
-            <p> Click to Upload a file (16: 19)</p>
-            <small>PNG, JPG, GIF up to 10MB</small>
+
+      {address ? (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              <div className="flex flex-col justify-evenly items-center">
+                <FiUploadCloud />
+                <p> Click to Upload a file (16: 19)</p>
+                <small>PNG, JPG, GIF up to 10MB</small>
+              </div>
+            </label>
+
+            <input
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) return true;
+                  return "Please upload one file";
+                },
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string); //cast to string since we know it's a tpe of string
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+            />
+            {/* calculation; cos we dontknow what the height could be, but we are rendering based off on the alloted width */}
+            {previewImage && (
+              <img
+                src={previewImage}
+                className="mt-4 object-cover"
+                style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+
+            {errors.image && <p> {errors.image.message} </p>}
           </div>
-        </label>
-
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          style={{ display: "none" }}
-          ref={register({
-            validate: (fileList: FileList) => {
-              if (fileList.length === 1) return true;
-              return "Please upload one file";
-            },
-          })}
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            if (event?.target?.files?.[0]) {
-              const file = event.target.files[0];
-              const reader = new FileReader();
-              reader.onloadend = () => {
-                setPreviewImage(reader.result as string); //cast to string since we know it's a tpe of string
-              };
-              reader.readAsDataURL(file);
-            }
-          }}
-        />
-        {/* calculation; cos we dontknow what the height could be, but we are rendering based off on the alloted width */}
-        {previewImage && (
-          <img
-            src={previewImage}
-            className="mt-4 object-cover"
-            style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
-          />
-        )}
-
-        {errors.image && <p> {errors.image.message} </p>}
-      </div>
-
-      <div className="mt-4">
-        <label htmlFor="bedrooms" className="block">
-          Beds
-        </label>
-        <input
-          type="number"
-          id="bedrooms"
-          name="bedrooms"
-          className="p-2"
-          ref={register({
-            required: "Please enter the number of bedrooms",
-            max: { value: 10, message: "Woooah, too big of a house" },
-            min: { value: 1, message: "Must have at least 1 bedroom" },
-          })}
-        />
-        {errors.bedrooms && <p> {errors.bedrooms.message} </p>}
-      </div>
-      <div className="mt-4">
-        <button
-          className="bg-green-500 hover:bg-green-700 font-bold py-2 px-4 rounded"
-          type="submit"
-          disabled={submitting}
-        >
-          Save
-        </button>{" "}
-        <Link href="/">
-          <a>Cancel</a>
-        </Link>
-      </div>
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              type="number"
+              id="bedrooms"
+              name="bedrooms"
+              className="p-2"
+              ref={register({
+                required: "Please enter the number of bedrooms",
+                max: { value: 10, message: "Woooah, too big of a house" },
+                min: { value: 1, message: "Must have at least 1 bedroom" },
+              })}
+            />
+            {errors.bedrooms && <p> {errors.bedrooms.message} </p>}
+          </div>
+          <div className="mt-4">
+            <button
+              className="bg-green-500 hover:bg-green-700 font-bold py-2 px-4 rounded"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>
+            <Link href="/">
+              <a>Cancel</a>
+            </Link>
+          </div>
+        </>
+      ) : null}
     </form>
   );
 };
