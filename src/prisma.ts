60507bd7a51984561a673d4d984103ed6ef0e16a
jeffascript <ofoaro.jeff@gmail.com>
2021-06-21 00:04:43 +0200

    [DB] Prisma initialised for client use

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,8 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; //created due to the yarn db:generate, specifically for our db
 
-export {};
+const prisma = new PrismaClient({
+  // log: process.env.NODE_ENV === "development" ? ["query", "info", "warn"]: []
+  log: ["query", "info", "warn"],
+});
+
+export { prisma, PrismaClient };
