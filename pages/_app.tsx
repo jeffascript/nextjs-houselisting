313f186442cfe62373c60e7e8fb6e566c001b8cc
jeffascript <ofoaro.jeff@gmail.com>
2021-06-25 21:22:19 +0200

    [ApolloClient] w/memoHooks & Provider init.for FE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// import { ApolloProvider } from "@apollo/client";
-// import { useApollo } from "src/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "src/apollo";
 import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const client = useApollo(); //Get instance of the Apollo created with the hook
+
   return (
     <AuthProvider>
-      <Head>
-        <title> House Listings | By Jeff</title>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        {/*/favicon.ico  will work since it is on the public folder as our baseurl is . on the settings */}
-      </Head>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Head>
+          <title> House Listings | By Jeff</title>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          {/*/favicon.ico  will work since it is on the public folder as our baseurl is . on the settings */}
+        </Head>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </AuthProvider>
   );
 }
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,5 +1,24 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
-// import { HttpLink } from "@apollo/client/link/http";
-// import { useMemo } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { HttpLink } from "@apollo/client/link/http";
+import { useMemo } from "react";
 
-export {};
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({ uri: "/api/graphql", credentials: "same-origin" }), //same-origin...will pass on the cookies and the headers from BE
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "cache-and-network",
+      },
+    },
+  });
+};
+
+/**
+ * Hook that gets an instance of the Apollo Client
+ */
+
+export function useApollo() {
+  const client = useMemo(() => createApolloClient(), []);
+  return client;
+}
